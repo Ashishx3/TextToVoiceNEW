1a1b6fb3b97eb1ff09a9de4b660eeae9fe5a94e1
Ashish <154009490+Ashishx3@users.noreply.github.com>
2024-12-12 22:57:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template, send_file
+from gtts import gTTS
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/synthesize', methods=['POST'])
+def synthesize():
+    text = request.form['text']
+    tts = gTTS(text)
+    tts.save("output.mp3")
+    return render_template('index.html', audio_path="output.mp3")
+
+@app.route('/download/<path:filename>')
+def download(filename):
+    return send_file(filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
